edd09158d4e3192bd5d5d305767f4f41f7a8fab6
Debojyoti Mondal <97456082+debojyoti31@users.noreply.github.com>
2022-12-21 21:30:37 +0530

    Update cyberbullying_detection.py

--- cyberbullying_detection.py
+++ cyberbullying_detection.py
@@ -532,9 +532,6 @@ vec = pickle.load(open('count_vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 fit_lenc = pickle.load(open('fit_lenc.pkl', 'rb'))
 
-n_vec = pickle.load(open('n_count_vectorizer.pkl', 'rb'))
-n_model = pickle.load(open('n_model.pkl', 'rb'))
-n_fit_lenc = pickle.load(open('n_fit_lenc.pkl', 'rb'))
 
 
 st.set_page_config(page_title="cyberbullying Detection", page_icon=":rotating_light:", layout="wide")
@@ -556,7 +553,6 @@ with st.container():
             - Ethnicity
             - Gender
             - Religion
-            - Other Cyberbullying 
             - Not Cyberbullying  
             """
         )
@@ -576,17 +572,10 @@ with st.container():
                 # predict
                 y_pred = model.predict(input_text1)
 
-                if fit_lenc.inverse_transform(y_pred)[0] != 'not_cyberbullying':
-                    # display
-                    st.write('**:red[Cyberbullying Type :]**')
-                    st.subheader(fit_lenc.inverse_transform(y_pred)[0])
-                    st.caption('This is only a Prediction based on Machine Learning, it may not be accurate.')
-                else:
-                    input_text2 = n_vec.transform([input_text]).toarray()
-                    n_y_pred = n_model.predict(input_text2)
-                    st.write('**:red[Cyberbullying Type :]**')
-                    st.subheader(n_fit_lenc.inverse_transform(n_y_pred)[0])
-                    st.caption('This is only a Prediction based on Machine Learning, it may not be accurate.')
+                # display
+                st.write('**:red[Cyberbullying Type :]**')
+                st.subheader(fit_lenc.inverse_transform(y_pred)[0])
+                st.caption('This is only a Prediction based on Machine Learning, it may not be accurate.')
                 
             else:
                 st.subheader('Please enter a text!')
