5adb4515229ab28ceb0726471999044dbd89e455
Debojyoti Mondal <97456082+debojyoti31@users.noreply.github.com>
2022-12-22 00:59:33 +0530

    Update cyberbullying_detection.py

--- cyberbullying_detection.py
+++ cyberbullying_detection.py
@@ -620,8 +620,8 @@ with st.container():
                             df.tweet.iloc[i] = GoogleTranslator(source='auto', target='en').translate(df.tweet.iloc[i])
                     df.tweet =  [preprocess_text(text) for text in df.tweet.values]
                     st.write('Translated Tweet Keywords :')
-                    df = df.dropna()
                     df.dropna(inplace=True)
+                    st.dataframe(df.tweet)
 
 
                     list_input = vec.transform(df.tweet.values).toarray()
